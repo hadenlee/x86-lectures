c89edd9a1f38ef716636dc707b8e9352f87035c5
haden <haden.lee@molocoads.com>
2021-04-12 14:08:16 -0700

    final changes

--- java/dataflow/src/main/java/edu/usfca/dataflow/L29/WindowingRecap.java
+++ java/dataflow/src/main/java/edu/usfca/dataflow/L29/WindowingRecap.java
@@ -6,6 +6,7 @@ import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.windowing.AfterPane;
+import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
 import org.apache.beam.sdk.transforms.windowing.FixedWindows;
 import org.apache.beam.sdk.transforms.windowing.Repeatedly;
 import org.apache.beam.sdk.transforms.windowing.Trigger;
@@ -24,37 +25,26 @@ public class WindowingRecap {
     //    example2();
     //    example3();
     //    example4Acc();
-    //    example4Dis();
+    //    example5Dis(); // <- change 'accumulating' to 'discarding'.
+    // exercise 6: just copy and paste example4Acc code, and then change the trigger object.
   }
 
-  public static void example4Dis() {
-    //(“A”, 1)	10:00AM
-    //(“A”, 2)	10:03AM
+  public static void example5Dis() {
     //(“B”, 3)	10:05AM
     //(“B”, 4)	10:08AM
-    //(“A”, 5)	10:11AM
-    //(“A”, 6)	10:13AM
     //(“B”, 7)	10:14AM
     //(“B”, 8)	10:21AM
-    //(“A”, 9)	10:21AM
     //(“B”, 10)	10:21AM
-    //(“A”, 11)	10:21AM
     Pipeline p = Pipeline.create();
     final long ts = Instant.parse("2021-05-01T10:00:00.000Z").toEpochMilli();
     final long minute = Duration.ofMinutes(1).toMillis();
 
     PCollection<KV<Long, KV<String, Integer>>> input = p.apply(Create.of(//
-      KV.of(ts, KV.of("A", 1)),//
-      KV.of(ts + 3 * minute, KV.of("A", 2)),//
       KV.of(ts + 5 * minute, KV.of("B", 3)),//
       KV.of(ts + 8 * minute, KV.of("B", 4)),//
-      KV.of(ts + 11 * minute, KV.of("A", 5)),//
-      KV.of(ts + 13 * minute, KV.of("A", 6)),//
       KV.of(ts + 14 * minute, KV.of("B", 7)),//
       KV.of(ts + 21 * minute, KV.of("B", 8)),//
-      KV.of(ts + 21 * minute, KV.of("A", 9)),//
-      KV.of(ts + 21 * minute, KV.of("B", 10)),//
-      KV.of(ts + 21 * minute, KV.of("A", 11))//
+      KV.of(ts + 21 * minute, KV.of("B", 10))//
     ));
 
     PCollection<KV<String, Integer>> timedData =
@@ -105,34 +95,23 @@ public class WindowingRecap {
   }
 
   public static void example4Acc() {
-    //(“A”, 1)	10:00AM
-    //(“A”, 2)	10:03AM
     //(“B”, 3)	10:05AM
     //(“B”, 4)	10:08AM
-    //(“A”, 5)	10:11AM
-    //(“A”, 6)	10:13AM
     //(“B”, 7)	10:14AM
     //(“B”, 8)	10:21AM
-    //(“A”, 9)	10:21AM
     //(“B”, 10)	10:21AM
-    //(“A”, 11)	10:21AM
     Pipeline p = Pipeline.create();
     final long ts = Instant.parse("2021-05-01T10:00:00.000Z").toEpochMilli();
     final long minute = Duration.ofMinutes(1).toMillis();
 
     PCollection<KV<Long, KV<String, Integer>>> input = p.apply(Create.of(//
-      KV.of(ts, KV.of("A", 1)),//
-      KV.of(ts + 3 * minute, KV.of("A", 2)),//
       KV.of(ts + 5 * minute, KV.of("B", 3)),//
       KV.of(ts + 8 * minute, KV.of("B", 4)),//
-      KV.of(ts + 11 * minute, KV.of("A", 5)),//
-      KV.of(ts + 13 * minute, KV.of("A", 6)),//
       KV.of(ts + 14 * minute, KV.of("B", 7)),//
       KV.of(ts + 21 * minute, KV.of("B", 8)),//
-      KV.of(ts + 21 * minute, KV.of("A", 9)),//
-      KV.of(ts + 21 * minute, KV.of("B", 10)),//
-      KV.of(ts + 21 * minute, KV.of("A", 11))//
+      KV.of(ts + 21 * minute, KV.of("B", 10))//
     ));
+
 
     PCollection<KV<String, Integer>> timedData =
       input.apply(ParDo.of(new DoFn<KV<Long, KV<String, Integer>>, KV<String, Integer>>() {
@@ -303,9 +282,9 @@ public class WindowingRecap {
         }
       }))//
       .apply(ParDo.of(new DoFn<KV<String, Integer>, KV<String, Integer>>() {
-        @ProcessElement public void process(ProcessContext c) {
-          System.out.format("[DoFn] key: %s  value: [%d]\ntimestamp:%s  pane: %s\n\n", c.element().getKey(),
-            c.element().getValue(), c.timestamp(), c.pane());
+        @ProcessElement public void process(ProcessContext c, BoundedWindow w) {
+          System.out.format("[DoFn] key: %s  value: [%d]\ntimestamp:%s  pane: %s  window: %s\n\n", c.element().getKey(),
+            c.element().getValue(), c.timestamp(), c.pane(), w.maxTimestamp());
           c.output(c.element());
         }
       })).apply(GroupByKey.create())//
